a15e63d570a24ee2dd84ecebb82fb6edfbc51326
andretecfil <andretecfil@gmail.com>
2017-10-13 17:56:45 +0100

    updates

--- Ch03/03_04/start/js/Note.js
+++ Ch03/03_04/start/js/Note.js
@@ -1,7 +1,15 @@
 var Note = React.createClass({
     render: function() {
-        
+        return (
+          <div className="note">
+            <span>
+              <button className="btn btn-primary glyphicon glyphicon-pencil" />
+              <button className="btn btn-primary glyphicon glyphicon-trash" />
+            </span>
+          </div>
+        );
+      }
 });
 
-React.render(<Note>Hello World</Note>, 
+React.render(<Note>Hello World</Note>,
     document.getElementById('react-container'));
